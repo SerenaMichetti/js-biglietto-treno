5f8ae99b495e03cdbe0a9bb2745e42c099dea213
Serena <sere.michetti@icloud.com>
2025-10-29 19:08:05 +0100

    creato prezzo senior scontato

--- main.js
+++ main.js
@@ -9,13 +9,16 @@ let priceJunior = 0
 if (age < 18) {
     priceJunior = priceBase - (0.20 * priceBase)
 }
- let priceSenior = 0
+let priceSenior = 0
+if (age > 65) {
+    priceSenior = priceBase - (0.40 * priceBase)
+}
 
 //output
-
 if (priceJunior !== 0) {
     alert('Il tuo biglietto costerà ' + priceJunior.toFixed(2) + '€')
-} 
-else {
+} else if (priceSenior !== 0) {
+    alert('Il tuo biglietto costerà ' + priceSenior.toFixed(2) + '€')
+} else {
     alert('Il tuo biglietto costerà ' + priceBase.toFixed(2) + '€')
 }
