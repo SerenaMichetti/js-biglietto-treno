392a8b95f375f0621a3e26746df15e94696a6ae0
Serena <sere.michetti@icloud.com>
2025-10-29 18:29:31 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//input
+const age = prompt ('Inserisci la tua età')
+const distance = prompt ('Inserisci i chilometri da parcorrere')
+
+//elaborazione
+const priceBase = distance * 0.21
+alert = priceBase
+
+
+
+//output
