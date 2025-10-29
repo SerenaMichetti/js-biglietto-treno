e2fbe5cd5bc3d388f8b2fb2d8d442aec125a3c2c
Serena <sere.michetti@icloud.com>
2025-10-29 19:01:35 +0100

    creato  prezzo scontato Junior

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 //input
-const age = prompt('Inserisci la tua età')
-const distance = prompt('Inserisci i chilometri da parcorrere')
+const age = parseInt(prompt('Inserisci la tua età'))
+const distance = parseInt(prompt('Inserisci i chilometri da parcorrere'))
+const price = 0.21
 
 //elaborazione
-const priceBase = distance * 0.21
-
-
+const priceBase = distance * price
+let priceJunior = 0
+if (age < 18) {
+    priceJunior = priceBase - (0.20 * priceBase)
+}
+ let priceSenior = 0
 
 //output
-alert('Il tuo biglietto costerà '+  priceBase + '€')
+
+if (priceJunior !== 0) {
+    alert('Il tuo biglietto costerà ' + priceJunior.toFixed(2) + '€')
+} 
+else {
+    alert('Il tuo biglietto costerà ' + priceBase.toFixed(2) + '€')
+}
