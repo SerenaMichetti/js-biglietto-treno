6ddea75c9ec4bb4e2f74d55da481af71992722fd
Serena <sere.michetti@icloud.com>
2025-10-29 18:35:40 +0100

    ceata prima parte con pèrezzo base

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //input
-const age = prompt ('Inserisci la tua età')
-const distance = prompt ('Inserisci i chilometri da parcorrere')
+const age = prompt('Inserisci la tua età')
+const distance = prompt('Inserisci i chilometri da parcorrere')
 
 //elaborazione
 const priceBase = distance * 0.21
-alert = priceBase
 
 
 
 //output
+alert('Il tuo biglietto costerà '+  priceBase + '€')
